310a1041e4c2d6e2e9307961cd865ee1445e6391
Jaidiver <jaidiver@gmail.com>
2019-10-09 10:11:00 -0500

    Cambio en documentación

--- demo/src/main/java/com/example/demo/Operations.java
+++ demo/src/main/java/com/example/demo/Operations.java
@@ -7,6 +7,13 @@ package com.example.demo;
  */
 public interface Operations {
 
+	/**
+	 * Sumatoria de dos números
+	 * @param a
+	 * @param b
+	 * @return la suma de a y b
+	 * @throws Exception
+	 */
 	public abstract int sum(final int a, final int b) throws Exception;
 	
 }
